d68710453fb8abb2a63c37ef30564aa7ac7f762b
Akihiro Maeda <sola.1980.a@gmail.com>
2012-11-23 20:15:40 +0900

    support jcrom.
    Change-Id: I83478d4efb42a4f8adb063c6ef9ac47988eff31e

--- device.mk
+++ device.mk
@@ -26,3 +26,5 @@ PRODUCT_COPY_FILES := \
 
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/maguro/device-vendor.mk)
+$(call inherit-product-if-exists, jcrom/common/common.mk)
+
