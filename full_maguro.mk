1648b535dc4c1a77da837e8da1361c6510d34ec6
Akihiro Maeda <sola.1980.a@gmail.com>
2013-01-26 01:15:21 +0900

    Update PRODUCT_BUILD_PROP_OVERRIDES ( 4.2 to 4.2.1 )

--- full_maguro.mk
+++ full_maguro.mk
@@ -37,7 +37,7 @@ $(call inherit-product, device/samsung/maguro/device.mk)
 #JCROM
 $(call inherit-product-if-exists, jcrom/samsung/maguro/device-maguro.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT=google/yakju/maguro:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.2 JOP40C 527662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT=google/yakju/maguro:4.2.1/JOP40D/533553:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.2.1 JOP40D 533553 release-keys"
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_maguro
